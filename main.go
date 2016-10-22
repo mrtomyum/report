7847d06903e978a54a5c0a872037f9a33684605c
Kasem Arnontavilas <mrtomyum@gmail.com>
2016-10-22 12:53:29 +0700

    Update Template for homepage

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
-	
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	DB_HOST = "tcp(nava.work:3306)"
+	DB_NAME = "sample"
+	DB_USER = "root"
+	DB_PASS = "mypass"
+)
+
+var DB *sqlx.DB
+
+func Home(c *gin.Context){
+
+	c.HTML(200,"index.tmpl", gin.H{
+		"title":"This is Home Page",
+		"id":"1",
+		"name":"tom",
+	})
+	//c.JSON(200, gin.H{"name":"This is Home Page"})
 }
+
+func main() {
+	var dsn = DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?parseTime=true"
+	DB = sqlx.MustConnect("mysql", dsn)
+
+	r := gin.Default()
+	r.LoadHTMLGlob("template/*")
+	r.GET("/", Home)
+
+	r.Run(":8080")
+}
+
